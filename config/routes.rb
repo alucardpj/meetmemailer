0e671af1ab7921bf84433338b8f7ecdc10d35de6
Jian Peng <alucardpj@gmail.com>
2012-06-14 17:37:50 +0800

    增加mailconfig, template, compaign, task资源，增加roo gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Meetmemailer::Application.routes.draw do
+  resources :compaigns do
+    resource :tasks
+  end
+
+  resources :templates
+
+  resources :mailconfigs
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Meetmemailer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'compaign#index'
 
   # See how all your routes lay out with "rake routes"
 
