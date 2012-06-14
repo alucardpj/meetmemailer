58f95a1741d6f515fac19f55cec81fc916c761f5
Jian Peng <alucardpj@gmail.com>
2012-06-14 16:20:17 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetmemailer::Application.config.secret_token = 'a01877a8c317414dc72d0cabb0183f1bdea6cc0658cf42d270ec1e46686988039349cc109db74cb290b17160f2089dc96966fa8056df3ec6dfa384d89d072446'
