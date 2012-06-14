fa8307d39239df55215e0b5da2f2294b39f5bab3
Jian Peng <alucardpj@gmail.com>
2012-06-14 18:09:15 +0800

    增加carrierwave gem，增加中文语言

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 
 gem 'jquery-rails'
 gem 'roo'
+gem 'carrierwave'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Meetmemailer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'compaign#index'
+  root :to => 'compaigns#index'
 
   # See how all your routes lay out with "rake routes"
 
